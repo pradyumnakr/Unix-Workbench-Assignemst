e895b3bae82ffa15d4014777c71f1219e91f3613
Pradyumna <pradyumnahebbar552@gmail.com>
2018-07-16 15:16:24 +0530

    files added

---
+++ guessinggame.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# File: guessinggame.sh
+
+dirarray=($(ls -d */))
+dircount=${#dirarray[@]}
+dirguess=0
+
+function checkguess {
+    # Usage of an if statement
+    if [[ $1 -lt $2 ]]
+    then
+    echo "echo Too low"
+    elif [[ $1 -gt $2 ]]
+    then
+    echo "echo Too high"
+    fi
+}
+
+# Usage of a loop
+while [ $dirguess -ne $dircount ]
+do
+echo "Guess the number of directories!"
+# Collecting user response
+read dirguess
+
+$(checkguess $dirguess $dircount)
+
